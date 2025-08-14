eeb70ef9654f25dd35b0a2b62798425daa30a060
OnkarBorude77 <onkar.wugwg22@sinhgad.edu>
2025-08-14 22:56:10 +0530

    remove static

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -87,7 +87,7 @@ USE_TZ = True
 
 # Static
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
