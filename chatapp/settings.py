1552be58a80318fe2e6fab364cf67897026eb837
OnkarBorude77 <onkar.wugwg22@sinhgad.edu>
2025-08-14 23:31:44 +0530

    .

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -60,10 +60,11 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get("REDIS_URL")],
+            "hosts": [os.environ.get("REDIS_URL", "redis://127.0.0.1:6379")],
         },
     },
 }
+
 
 
 # DB 
