d2a0b7294ff862391c199694ce56ec3d673f47e2
OnkarBorude77 <onkar.wugwg22@sinhgad.edu>
2025-08-14 22:51:11 +0530

    .

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -86,8 +86,8 @@ USE_I18N = True
 USE_TZ = True
 
 # Static
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
